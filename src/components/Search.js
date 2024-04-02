a4b678fdd1a0142213a14fa9c1c8148302083aae
Kyrene Sugianto <66699981+kyrenesugianto@users.noreply.github.com>
2024-04-02 19:30:08 -0400

    Update Search.js
    Changed to use Menu.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,45 +1,44 @@
 import './Search.css';
-import {useState} from 'react';
+import { useState } from 'react';
 import SearchIcon from '@mui/icons-material/Search';
-import {grey} from '@mui/material/colors';
-import "@fontsource/league-spartan/800.css";
-import { PropaneSharp } from '@mui/icons-material';
-import {Link} from 'react-router-dom'
+import { grey } from '@mui/material/colors';
+import { Link } from 'react-router-dom';
+import CustomMenu from './Menu.js'; // Import CustomMenu component
 
-export default function Search(props){
+export default function Search(props) {
   const [query, setQuery] = useState('');
   const [results, setResults] = useState([]);
 
   const handleSearch = async () => {
     try {
-     // const response = await axios.post('/search', { query });
+      // const response = await axios.post('/search', { query });
       setResults('');
     } catch (error) {
       console.error('Error searching:', error);
     }
   };
-    return (
-        <div className="container"> {/* Add a container class */}
-          <div className="top-bar">
-            <div className="menu-icon">
-              <div className="menu-line"></div>
-              <div className="menu-line"></div>
-              <div className="menu-line"></div>
-            </div> {/* Add a div for the menu icon */}
-          </div>
-          <img src='../../Puma.png' className="pumaLogo"/>  
-          <h2 className="title">Search a Class</h2> {/* Add a title class */}
-          <div className='search-div'>
-          <input
-            type="text"
-            value={query}
-            onChange={(e) => setQuery(e.target.value)}
-            placeholder="Lookup a class..."
-            className="search-bar" // Search-bar class here
-          />
-         <Link to='/map'><button className='searchIcon' onClick={props.handleSearch}><SearchIcon sx={{color: grey[50]}} fontSize='large'/></button></Link>
-          </div>
-         
-        </div>
-      );
+
+  return (
+    <div className="container">
+      {/* CustomMenu component */}
+      <CustomMenu />
+      {/* Original component */}
+      <img src="../../Puma.png" className="pumaLogo" />
+      <h2 className="title">Search a Class</h2>
+      <div className="search-div">
+        <input
+          type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder="Lookup a class..."
+          className="search-bar"
+        />
+        <Link to="/map">
+          <button className="searchIcon" onClick={handleSearch}>
+            <SearchIcon sx={{ color: grey[50] }} fontSize="large" />
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 }
