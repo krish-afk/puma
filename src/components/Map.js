9878d9a5817cc546f27a7573539ef6b0346f9044
padfoot2803 <118225759+padfoot2803@users.noreply.github.com>
2024-04-03 21:47:41 -0400

    Update Map.js
    There are now 2 Map.js files in this patch that have the same code.

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,17 +3,16 @@ import { useState, useEffect } from 'react';
 import "@fontsource/league-spartan/800.css";
 import Course from './CourseClass';
 import axios from 'axios';
+import { CastRounded } from '@mui/icons-material';
 
 export default function Map() {
     const [result, setResults] = useState([]);
     const [loading, setLoading] = useState(true); // Add loading state
-
+    let link = 'http://localhost:8000/getClass?course=CS230'
     useEffect(() => {
         const fetchData = async () => {
             try {
-                console.log("Searching");
-                const response = await axios.get('http://localhost:8000/getClass?course=CS110');
-                console.log(response.data);
+                const response = await axios.get(link);
                 setResults(response.data);
                 setLoading(false); // Set loading to false after data is fetched
             } catch (error) {
@@ -31,25 +30,23 @@ export default function Map() {
 
     // Check if result and result.courseInfo exist before accessing Prerequisites
     var prereqs = result && result.courseInfo ? result.courseInfo.Prerequisites : [];
-    console.log("PPREREQ", prereqs);
+    //console.log("PREREQS", prereqs); //this is to print the prereqs.
 
     // example that would be returned in query:
-    var courseName = result && result.courseInfo ? result.courseInfo.Name : "";
-    var parentCourse = new Course(220, "CS", "Programming Methodology", "...", prereqs && prereqs.length > 0 ? prereqs[0][0] : []);
-
+    console.log(result.courseInfo.Name)
+    var parentCourse = new Course(link.substring(40, 43), "CS", result.courseInfo.Name.substring(9), "...", prereqs && prereqs.length > 0 ? prereqs: []);
     const results = [parentCourse];
-    console.log(results)
     return (
         <div className="prereq-list">
             <ul>
                 {results.map((course) => (
-                    <h3 key={course.id} className="course-name">{courseName}</h3>
+                    <h3 key={course.id} className="course-name">{course.dept + course.id + " - " + course.name}</h3>
                 ))}
             </ul>
             <div className="container">
                 <ul>
                     {results.map((course) => (
-                        <p key={course.id}>Prerequisites: {course.prereqs}</p>
+                        <p key={course.id}>Prerequisites:<br></br> {course.prereqs.map(list => list.map(set => set[0] + " "))}</p>
                     ))}
                 </ul>
                 <div className="top-bar">
@@ -63,3 +60,4 @@ export default function Map() {
         </div>
     );
 }
+
