bb9e80acf7528aa331d6b8070a09e0a21d85a431
Kyrene Sugianto <66699981+kyrenesugianto@users.noreply.github.com>
2024-04-04 20:15:33 -0400

    Update Map.js including menu icon and Keshav's map

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,18 +3,31 @@ import { useState, useEffect } from 'react';
 import "@fontsource/league-spartan/800.css";
 import Course from './CourseClass';
 import axios from 'axios';
-import { CastRounded } from '@mui/icons-material';
+//import { CastRounded } from '@mui/icons-material';
 import CustomMenu from './Menu'; // Import the CustomMenu component
+import './Tree.css' // Import Tree css
+
+var treeData = []
+
+const searchResult = 'CS709'; // hard coded but doesn't have to be
 
 
 export default function Map() {
     const [result, setResults] = useState([]);
     const [loading, setLoading] = useState(true); // Add loading state
-    let link = 'http://localhost:8000/getClass?course=COMPSCI220'
+    
     useEffect(() => {
-        const fetchData = async () => {
+        const fetchData = async (searchCourse) => {
             try {
-                const response = await axios.get(link);
+                console.log("Searching");
+                const response = await axios.get('http://localhost:8000/getClass?course='+searchCourse);
+                //console.log(response.data);
+                //console.log(response.data.courseInfo.Prerequisites)
+                
+
+                response.data.courseInfo.Prerequisites = await response.data.courseInfo.Prerequisites.forEach(e => fetchChildCourse(e));
+                console.log(response.data.courseInfo.Prerequisites)
+                
                 setResults(response.data);
                 setLoading(false); // Set loading to false after data is fetched
             } catch (error) {
@@ -22,8 +35,23 @@ export default function Map() {
             }
         };
 
-        fetchData();
+        const fetchChildCourse = async (searchCourse) => {
+            try {
+                console.log("Searching for prereq");
+                const response = await axios.get('http://localhost:8000/getClass?course='+searchCourse);
+                
+                if (response.data.courseInfo.Prerequisites.length !== 0){
+                    response.data.courseInfo.Prerequisites = await response.data.courseInfo.Prerequisites.forEach(e => fetchChildCourse(e));
+                }
+                return response.data;
+
+            } catch (error) {
+                console.error('Error searching:', error);
+            }
+        };
+        fetchData(searchResult);
     }, []);
+
 
     if (loading) {
         // Render loading state while data is being fetched
@@ -32,29 +60,86 @@ export default function Map() {
 
     // Check if result and result.courseInfo exist before accessing Prerequisites
     var prereqs = result && result.courseInfo ? result.courseInfo.Prerequisites : [];
-    //console.log("PREREQS", prereqs); //this is to print the prereqs.
+    //console.log("PPREREQ", prereqs);
 
     // example that would be returned in query:
-    console.log(result.courseInfo.Name)
-    var parentCourse = new Course(link.substring(40, 43), "CS", result.courseInfo.Name.substring(9), "...", prereqs && prereqs.length > 0 ? prereqs: []);
+    var courseName = result && result.courseInfo ? result.courseInfo.Name : "";
+    var parentCourse = new Course(220, "CS", "Programming Methodology", "...", prereqs && prereqs.length > 0 ? prereqs[0][0] : []);
+
+    console.log(result.courseInfo.Prerequisites)
+    createTree(result.courseInfo);
+
     const results = [parentCourse];
+    //console.log(result)
     return (
         <div className="prereq-list">
             <ul>
                 {results.map((course) => (
-                    <h3 key={course.id} className="course-name">{course.dept + course.id + " - " + course.name}</h3>
+                    <h3 key={course.id} className="course-name">{courseName}</h3>
                 ))}
             </ul>
             <div className="container">
+                <div className="tree">
+                    {
+                        treeRendering(treeData)
+                    }               
+                </div>
                 <ul>
                     {results.map((course) => (
-                        <p key={course.id}>Prerequisites:<br></br> {course.prereqs.map(list => list.map(set => set[0] + " "))}</p>
+                        <p key={course.id}> </p>//Prerequisites: {course.prereqs}</p> 
                     ))}
                 </ul>
                 <div className="top-bar">
-                    <CustomMenu /> {/* Render the CustomMenu component */}
+                    <CustomMenu/>
                 </div>
             </div>
         </div>
     );
+}
+
+function createTree(courseInfo){
+   treeData = [];
+   treeData.push(createNode(courseInfo));
+}
+
+function createNode(courseInfo){
+    
+    if (courseInfo.Prerequisites = []){ // should be (courseInfo.Prerequisites.length === 0){ 
+        return {
+            id: courseInfo.id,
+            name: courseInfo.Name,
+            diamond: false,
+            children: []
+        }
+    }
+    else{
+        var prereqs = [];
+        courseInfo.Prerequisites.forEach(course => prereqs.push(createNode(course)));
+        return {
+            id: courseInfo.id,
+            name: courseInfo.name,
+            diamond: false,
+            children: prereqs
+        }
+    }
+}
+
+const treeRendering = (treeData) => {
+    
+    return (
+        <ul>
+        {
+            treeData.map((item)=>                
+                <li className={item.name+item.id}>
+                    <div>{item.name}</div>
+                    {
+                        item.children && item.children.length ?
+                        treeRendering(item.children)
+                        :''
+                    }
+                </li>
+            )                    
+        }
+        </ul>
+    )
 }
