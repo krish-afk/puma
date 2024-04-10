f16b909fde042a102960c0c59c1a7d1b42ca101d
krish-afk <84220215+krish-afk@users.noreply.github.com>
2024-04-10 01:10:12 -0400

    Delete backend directory

--- backend/routes/classes.js
+++
@@ -1,6 +0,0 @@
-const express= require('express');
-const router= express.Router();
-const controller= require('../controllers/classes')
-
-router.get('/getClass',controller.getClasses)
-module.exports= router;
