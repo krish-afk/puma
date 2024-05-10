27127e9a42d4be090157c4591f8b756aa59114d7
Noah F <nhford@umass.edu>
2024-05-10 19:33:29 -0400

    integration testing including popup

--- src/App.spec.jsx
+++ src/App.spec.jsx
@@ -2,49 +2,53 @@ import { test, expect } from '@playwright/experimental-ct-react';
 import App from './App';
 // import Search from 'src/components/Search.js';
 
-test.use({ viewport: { width: 500, height: 500 } });
+test.use({ viewport: { width: 1000, height: 1000 } });
 
 ////////////////////////////////////////////////
-// Complete Integration Test
+// Integration Test of Course Search Use Case
 ////////////////////////////////////////////////
 test('round trip integration test', async ({ mount,page }) => {
   const component = await mount(<App />);
 
-  // through log-in
+  // Navigating the Log-In Front-End
+  await expect(component).toContainText('Log In');
   await page.keyboard.press('Tab')
-  await page.keyboard.insertText('No')
+  // username and password pair, 'Noo' & 'Foo' valid in the database
+  await expect(component).toContainText('Enter Username');
+  await page.keyboard.insertText('Noo')
   await page.keyboard.press('Tab')
-  await page.keyboard.insertText('Fo')
+  await expect(component).toContainText('Enter Password');
+  await page.keyboard.insertText('Foo')
   await page.keyboard.press('Tab')
+  await expect(component).toContainText('Log In');
   await page.keyboard.press('Enter')
 
   // course search
   await expect(component).toContainText('Search a Class');
-  await page.keyboard.press('Tab')
+  await page.keyboard.press('Tab') // hamburger menu
   await page.keyboard.press('Tab')
   await page.keyboard.insertText('CS220')
   await page.keyboard.press('Enter')
 
   // course map
-  await expect(component).toContainText('CS 220');
+  await expect(component).toContainText('CS 220 — Programming Methodology');
   // children nodes should be present
   await expect(component).toContainText('CICS 210');
   await expect(component).toContainText('CICS 160');
 
-  // search new query
-  await page.keyboard.press('Tab')
-  await page.keyboard.press('Tab')
-  await page.keyboard.press('Tab')
-  await page.keyboard.insertText('CICS110')
-  await page.keyboard.press('Enter')
-
-  // new course map
-  await expect(component).toContainText('CICS 110');
-  // children nodes should be present
-  await expect(component).toContainText('MATH 101');
-  await expect(component).toContainText('MATH 102');
+  // test pop-up
+  const parent = await page.$('li');
+  expect(parent).not.toBe(null);
+  const firstDivInLi = await parent.$('div');
+  expect(firstDivInLi).not.toBe(null);
+  await firstDivInLi.click();
+  const popupBody = await page.waitForSelector('.popup-body');
+  expect(popupBody).not.toBe(null);
+  const popupBodyText = await popupBody.innerText();
+  expect(popupBodyText).toContain('The goal of COMPSCI 220');
 
 });
+
 
 ////////////////////////////////////////////////
 // Unit Testing of Log-In page
